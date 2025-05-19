09dc81ef490966443968b02d4bb020ee76e8c934
saiswarya4321 <aiswaryas4906@gmail.com>
2025-05-19 19:37:00 +0530

    password utility changed

--- utilities/passwordUtilities.js
+++ utilities/passwordUtilities.js
@@ -1,13 +1,30 @@
-const bcrypt=require("bcrypt")
+// const bcrypt=require("bcrypt")
 
 
-const hashPassword=async(password)=>{
-    const salt= await bcrypt.genSalt()
-    const hashedPassword=await bcrypt.hash(password,salt);
-    return hashedPassword;
-}
-const comparePassword= async(password,hashedPassword)=>{
-    const passwordMatch= await bcrypt.compare(password,hashedPassword)
-    return passwordMatch;
-}
-module.exports={hashPassword,comparePassword}
+// const hashPassword=async(password)=>{
+//     const salt= await bcrypt.genSalt()
+//     const hashedPassword=await bcrypt.hash(password,salt);
+//     return hashedPassword;
+// }
+// const comparePassword= async(password,hashedPassword)=>{
+//     const passwordMatch= await bcrypt.compare(password,hashedPassword)
+//     return passwordMatch;
+// }
+// module.exports={hashPassword,comparePassword}
+
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10; // ⚙️ Adjust this to 8 or 9 if you need faster hashing
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+};
+
+const comparePassword = async (password, hashedPassword) => {
+  const passwordMatch = await bcrypt.compare(password, hashedPassword);
+  return passwordMatch;
+};
+
+module.exports = { hashPassword, comparePassword };
